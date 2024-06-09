15074b9050cc91336765d8fccc937e5492243afc
Talha Asif <98359013+TalhahAsif@users.noreply.github.com>
2024-06-09 06:21:52 +0500

    solving data in local Storage

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-// mainPage.style.display = "none"
+const user_email = localStorage.getItem("email");
 
-console.log(mainPage);
+noNotes.style.display = "none"
 
 const setEmail = () => {
-  console.log(email);
   if (!email.value || !password.value)
     return alert("Please Enter You password or Email.");
   localStorage.setItem("email", email.value);
   check_user_login();
+  email.value = "";
+  password.value = "";
 };
 
 const check_user_login = () => {
@@ -22,12 +23,62 @@ const check_user_login = () => {
   }
 };
 
-
 const logout = () => {
-    localStorage.clear("email")
-    check_user_login()
+  localStorage.clear("email");
+  check_user_login();
 };
+
+const setNotestoLocalStorage = () => {
+  const detailsObj = {
+    email: user_email,
+    subject: subjectInput.value,
+    noteText: noteInput.value,
+  };
+  settingValuetolocalstorage(detailsObj)
+};
+
+const settingValuetolocalstorage = (detailsObj) => {
+ const notes = localStorage.getItem("notesDetails")
+
+ if (notes) {
+    noNotes.style.display = "block"
+ }
+};
+
+const addnote = () => {
+  const noteListValue = `<li class="py-3 my-3 px-5 rounded bg-slate-800">
+<div class="flex justify-between">
+  <p
+    id="liSubject"
+    class="text-sm w-14 text-center rounded-full px-1 my-1 bg-red-600"
+  >
+    ${subjectInput.value}
+  </p>
+  <div>
+    <i class="fa-solid fa-trash mx-3 cursor-pointer"></i>
+    <i class="fa-regular fa-pen-to-square cursor-pointer"></i>
+  </div>
+</div>
+<p class="text-lg">
+  ${noteInput.value}
+</p>
+</li>`;
+
+  localStorage.setItem("notesDetails", detailsObj);
+
+  if (!noteInput.value || !subjectInput.value) {
+    alert("Please Fill all fields");
+  } else {
+    noteList.innerHTML = noteListValue;
+  }
+
+  console.log(noteInput.value);
+  noteInput.value = "";
+  subjectInput.value = "";
+};
+
+check_user_login();
 
 loginBtn.addEventListener("click", setEmail);
 logoutBTN.addEventListener("click", logout);
-
+addNoteBTN.addEventListener("click", addnote);
