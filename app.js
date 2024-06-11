8636d34cb369af0cc6b32d705af22adbaedd22d0
Talha Asif <98359013+TalhahAsif@users.noreply.github.com>
2024-06-12 00:25:57 +0500

    solving getting specific user date

--- app.js
+++ app.js
@@ -35,55 +35,77 @@ const savingNoteinLS = () => {
     noteText: noteInput.value,
   };
 
-  const notes = localStorage.getItem("notes");
-
-  if (notes) {
-    const userNotesArray = localStorage.getItem("notes");
-    const userNotes = JSON.parse(userNotesArray);
-    userNotes.push(detailsObj);
-    localStorage.setItem("notes", JSON.stringify(userNotes));
+  if (!noteInput.value || !subjectInput.value) {
+    alert("Please Fill all fields");
   } else {
-    const newNote = [detailsObj];
-    localStorage.setItem("notes", JSON.stringify(newNote));
+    const notes = localStorage.getItem("notes");
+    if (notes) {
+      noteList.innerHTML = "";
+      subjects.innerHTML = "";
+      const userNotesArray = localStorage.getItem("notes");
+      const userNotes = JSON.parse(userNotesArray);
+      userNotes.push(detailsObj);
+      localStorage.setItem("notes", JSON.stringify(userNotes));
+    } else {
+      const newNote = [detailsObj];
+      localStorage.setItem("notes", JSON.stringify(newNote));
+    }
+    subjectInput.value = "";
+    noteInput.value = "";
   }
-  subjectInput.value = "";
-  noteInput.value = "";
+  showNotes();
 };
 
-// const addnote = () => {
+const notes = localStorage.getItem("notes");
 
+console.log(JSON.parse(notes));
 
+const showNotes = () => {
+  const notes = localStorage.getItem("notes");
+  const userEmail = localStorage.getItem("email");
 
-//   const noteListValue = `<li class="py-3 my-3 px-5 rounded bg-slate-800">
-// <div class="flex justify-between">
-//   <p
-//     id="liSubject"
-//     class="text-sm w-14 text-center rounded-full px-1 my-1 bg-red-600"
-//   >
-//     ${subjectInput.value}
-//   </p>
-//   <div>
-//     <i class="fa-solid fa-trash mx-3 cursor-pointer"></i>
-//     <i class="fa-regular fa-pen-to-square cursor-pointer"></i>
-//   </div>
-// </div>
-// <p class="text-lg">
-//   ${noteInput.value}
-// </p>
-// </li>`;
+  const LSnotesArray = JSON.parse(notes);
 
-//   // localStorage.setItem("notes  Details", detailsObj);
+  if (!LSnotesArray) {
+    noNotes.style.display = "block";
+    noteList.style.display = "none";
+  } else {
+    noNotes.style.display = "none";
+    noteList.style.display = "block";
+    LSnotesArray.forEach(function (value, index) {
+      if (value.email == userEmail) {
+        const subjectList = `<option>${value.subject}</option>`;
+        const noteListValue = `<li class="py-3 my-3 px-5 rounded bg-slate-800">
+                  <div class="flex justify-between">
+                  <p
+                  id="liSubject"
+                  class="text-sm w-14 text-center rounded-full px-1 my-1 bg-red-600"
+                  >
+                  ${value.subject}
+                  </p>
+                  <div>
+                  <i class="fa-regular fa-pen-to-square cursor-pointer"></i>
+                  <i class="fa-solid fa-trash mx-3 cursor-pointer"></i>
+                  </div>
+                  </div>
+                  ${LSnotesArray[index].noteText}
+                  <p class="text-lg">
+  </p>
+  </li>`;
+      }
 
-//   if (!noteInput.value || !subjectInput.value) {
-//     alert("Please Fill all fields");
-//   } else {
-//     noteList.innerHTML = noteListValue;
-//   }
+      subjects.innerHTML += subjectList;
+      noteList.innerHTML += noteListValue;
+    });
+  }
 
-//   console.log(noteInput.value);
-// };
+  // localStorage.setItem("notes  Details", detailsObj);
+
+  // console.log(noteInput.value);
+};
 
 check_user_login();
+showNotes();
 
 loginBtn.addEventListener("click", setEmail);
 logoutBTN.addEventListener("click", logout);
